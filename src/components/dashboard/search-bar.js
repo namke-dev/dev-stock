462c7da531da4b25ee871dae745ab298d1f605c9
namke-dev <namke.dev@gmail.com>
2024-01-08 17:14:18 +0700

    replace lable search bar

--- src/components/dashboard/search-bar.js
+++ src/components/dashboard/search-bar.js
@@ -4,7 +4,7 @@ import { FaSearch } from "react-icons/fa";
 export default function SearchBar() {
   return (
     <div className="flex flex-col md:flex-row px-2 md:items-center justify-center md:justify-start h-full w-full md:gap-4">
-      <p>Search</p>
+      <p>Search symbol</p>
       <div
         className="flex items-center
         md:my-4 border-2
@@ -18,7 +18,7 @@ export default function SearchBar() {
         <input
           type="text"
           className="mx-3 focus:outline-0 flex flex-grow text-gray-900"
-          placeholder="Search stock, ex: google"
+          placeholder="ex: google"
         />
 
         {/* Search button */}
