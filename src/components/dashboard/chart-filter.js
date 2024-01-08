7332bb4981c1463784fbd225295e1624ddceb228
namke-dev <namke.dev@gmail.com>
2024-01-08 16:18:08 +0700

    add chart filter css

--- src/components/dashboard/chart-filter.js
+++ src/components/dashboard/chart-filter.js
@@ -4,15 +4,13 @@ export default function ChartFilter({ text, isActive, onClick }) {
   return (
     <button
       onClick={onClick}
-      className={`w-12 h-8 m-2 border-1 
+      className={`w-10 h-6 m-2 border-1 
       rounded-md
       flex items-center justify-center
       cursor-pointer
-      ${
-        isActive
-          ? "bg-indigo-600 border-indigo-700 text-gray-100"
-          : "text-indigo-300 border-indigo-300"
-      }`}
+      border border-indigo-500
+      text-sm
+      ${isActive ? "bg-indigo-500 text-gray-50" : "text-indigo-500"}`}
     >
       {text}
     </button>
