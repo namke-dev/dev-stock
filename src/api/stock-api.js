3fff1b6134e023fd9b5a962483ee8542d66af402
namke-dev <namke.dev@gmail.com>
2024-01-08 19:22:17 +0700

    config api call

--- src/api/stock-api.js
+++ src/api/stock-api.js
@@ -5,13 +5,13 @@ const BASE_URL = "https://real-time-finance-data.p.rapidapi.com";
 const options = {
   url: BASE_URL,
   headers: {
-    "X-RapidAPI-Key": process.env.REACT_APP_API_KEY,
+    "X-RapidAPI-Key": process.env.NEXT_PUBLIC_REACT_APP_API_KEY,
     "X-RapidAPI-Host": "real-time-finance-data.p.rapidapi.com",
   },
 };
 
 export const fetchFromApi = async (url) => {
-  console.log("key: " + process.env.REACT_APP_API_KEY);
+  console.log("key: " + process.env.NEXT_PUBLIC_REACT_APP_API_KEY);
   console.log(`Call api => ${BASE_URL}/${url}`);
 
   const { data } = await axios.get(`${BASE_URL}/${url}`, options);
