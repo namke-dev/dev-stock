85ddcf493dffa4d30efb91b07ef5824ccbe9289d
namke-dev <namke.dev@gmail.com>
2024-01-09 01:25:55 +0700

    clean unuse feature

--- src/components/dashboard/option-bar.js
+++ src/components/dashboard/option-bar.js
@@ -5,32 +5,26 @@ export default function OptionBar() {
   return (
     <div
       className="flex flex-col items-start justify-center h-full px-2 gap-1
-      text-sm "
+      text-sm md:text-base"
     >
-      <div className="flex flex-col md:flex-row w-full">
-        <p className="flex justify-start mb-2 md:m-0">Feature</p>
-        <ul className="flex flex-row flex-wrap gap-2 text-gray-700 md:ml-12">
-          {Object.values(PAGE_FEATURE).map((option) => {
-            return (
-              <li
-                key={option}
-                className="rounded-lg bg-gray-100 px-2 border hover:border-black/85 hover:bg-gray-200 cursor-pointer"
-              >
-                {option}
-              </li>
-            );
-          })}
-        </ul>
-      </div>
-
-      <div className="flex flex-col md:flex-row w-full mb-2 md:mb-0">
-        <p className="flex justify-start mt-2 mb-1 md:m-0">Market trend</p>
-        <ul className="flex flex-row flex-wrap gap-2 text-gray-700 md:ml-12">
+      <div className="flex flex-col md:flex-row w-full mb-2 md:mb-0 md:items-center">
+        <p
+          className="flex justify-start 
+          mt-2 mb-1 md:m-0
+          font-semibold
+          pb-1"
+        >
+          Market trend
+        </p>
+        <ul className="flex flex-row flex-wrap gap-2 md:gap-3 text-gray-700 md:ml-9">
           {Object.values(MARKET_TREND_TYPE).map((option) => {
             return (
               <li
                 key={option}
-                className="rounded-lg bg-gray-100 px-2 border hover:border-black/85 hover:bg-gray-200 cursor-pointer"
+                className="rounded-md bg-gray-100 px-2 
+                border border-black/30 
+                hover:text-gray-50 hover:bg-gray-600 
+                cursor-pointer"
               >
                 {option}
               </li>
