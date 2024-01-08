e3ff570aab06f42ae49c7efd231ccd4311d444a2
namke-dev <namke.dev@gmail.com>
2024-01-08 17:27:52 +0700

    fix responsive

--- src/components/dashboard/option-bar.js
+++ src/components/dashboard/option-bar.js
@@ -24,7 +24,7 @@ export default function OptionBar() {
       </div>
 
       <div className="flex flex-col md:flex-row w-full mb-2 md:mb-0">
-        <p className="flex justify-start mt-3 mb-2 md:m-0">Market trend</p>
+        <p className="flex justify-start mt-2 mb-1 md:m-0">Market trend</p>
         <ul className="flex flex-row flex-wrap gap-2 text-gray-700 md:ml-12">
           {Object.values(MARKET_TREND_TYPE).map((option) => {
             return (
