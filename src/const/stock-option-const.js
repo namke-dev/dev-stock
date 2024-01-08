5782e2d52c7cdeced025d1a0be569147782c305e
namke-dev <namke.dev@gmail.com>
2024-01-08 10:43:31 +0700

    Create Mock option

---
+++ src/const/stock-option-const.js
@@ -0,0 +1,9 @@
+export const MARKET_TREND_TYPE = {
+  MARKET_INDEXES: "Market Index",
+  MOST_ACTIVE: "Most Active",
+  GAINERS: "Gainers",
+  LOSERS: "Loser",
+  CRYPTO: "Crypto",
+  CURRENCIES: "Currencies",
+  CLIMATE_LEADERS: "Climate Leader",
+};
