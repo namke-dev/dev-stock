d8333e770d762222b9b4220f859dfb5c30a8cb93
namke-dev <namke.dev@gmail.com>
2024-01-11 00:03:58 +0700

    add ooption bar

--- src/components/dashboard/candle-chart.js
+++ src/components/dashboard/candle-chart.js
@@ -1,26 +1,28 @@
 import React, { useEffect, useState } from "react";
 import dynamic from "next/dynamic";
-
-const Chart = dynamic(() => import("react-apexcharts"), { ssr: false });
+const ReactApexChart = dynamic(() => import("react-apexcharts"), {
+  ssr: false,
+});
 
 const CandleChart = ({ chartData }) => {
   const [chartOptions, setChartOptions] = useState({});
   const [domLoaded, setDomLoaded] = useState(false);
 
   console.log("chartData");
-  console.log(chartData);
   if (!chartData) return;
 
   useEffect(() => {
-    const formatedData = Object.entries(chartData).map(([date, values]) => ({
-      x: new Date(date).getTime(),
-      y: [
-        parseFloat(values["1. open"]),
-        parseFloat(values["2. high"]),
-        parseFloat(values["3. low"]),
-        parseFloat(values["4. close"]),
-      ],
-    }));
+    const formatedPriceData = Object.entries(chartData).map(
+      ([date, values]) => ({
+        x: new Date(date).getTime(),
+        y: [
+          parseFloat(values["1. open"]),
+          parseFloat(values["2. high"]),
+          parseFloat(values["3. low"]),
+          parseFloat(values["4. close"]),
+        ],
+      })
+    );
 
     const formatedVolumeData = Object.entries(chartData).map(
       ([date, values]) => ({
@@ -32,36 +34,133 @@ const CandleChart = ({ chartData }) => {
     setChartOptions({
       series: [
         {
-          data: formatedData,
+          name: "candlestick",
+          data: formatedPriceData,
         },
       ],
-      chart: {
-        type: "candlestick",
-        height: 350,
+      options: {
+        chart: {
+          type: "candlestick",
+          height: 290,
+          id: "candles",
+          toolbar: {
+            autoSelected: "pan",
+            show: false,
+          },
+          zoom: {
+            enabled: false,
+          },
+        },
+        plotOptions: {
+          candlestick: {
+            colors: {
+              upward: "#00CC00",
+              downward: "#FF3333",
+            },
+          },
+        },
+        xaxis: {
+          type: "datetime",
+        },
+        yaxis: {
+          tooltip: {
+            enabled: true,
+          },
+          labels: {
+            formatter: function (value) {
+              // Format the y-axis labels to a fixed number of decimal places
+              return value.toFixed(0);
+            },
+          },
+          forceNiceScale: true, // Ensure that the y-axis labels are nicely rounded
+        },
       },
-      xaxis: {
-        type: "datetime",
-      },
-      yaxis: {
-        tooltip: {
-          enabled: true,
+      seriesBar: [
+        {
+          name: "volume",
+          data: formatedVolumeData,
+        },
+      ],
+      optionsBar: {
+        chart: {
+          height: 160,
+          type: "bar",
+          brush: {
+            enabled: true,
+            target: "candles",
+          },
+          selection: {
+            enabled: true,
+            xaxis: {
+              min: new Date(Object.keys(chartData)[0]).getTime(),
+              max: new Date(
+                Object.keys(chartData)[Object.keys(chartData).length - 1]
+              ).getTime(),
+            },
+            fill: {
+              color: "#ccc",
+              opacity: 0.4,
+            },
+            stroke: {
+              color: "#0D47A1",
+            },
+          },
+        },
+        dataLabels: {
+          enabled: false,
+        },
+        plotOptions: {
+          bar: {
+            columnWidth: "80%",
+            color: "#F15B46",
+          },
+        },
+        stroke: {
+          width: 0,
+        },
+        xaxis: {
+          type: "datetime",
+          axisBorder: {
+            offsetX: 13,
+          },
+        },
+        yaxis: {
+          labels: {
+            show: false,
+          },
         },
       },
     });
+
     setDomLoaded(true);
   }, [chartData]);
 
   return (
     <div className="mixed-chart h-full w-full">
-      {typeof window !== "undefined" && domLoaded && (
-        <Chart
-          options={chartOptions}
-          series={chartOptions.series}
-          type="candlestick"
-          width={"98%"}
-          height={"98%"}
-        />
-      )}
+      <div className="chart-box h-full w-full">
+        <div id="chart-candlestick" className=" h-2/3 w-full">
+          {typeof window !== "undefined" && domLoaded && (
+            <ReactApexChart
+              options={chartOptions.options}
+              series={chartOptions.series}
+              type="candlestick"
+              height={"100%"}
+              width={"100%"}
+            />
+          )}
+        </div>
+        <div id="chart-bar" className=" h-1/3 w-full">
+          {typeof window !== "undefined" && domLoaded && (
+            <ReactApexChart
+              options={chartOptions.optionsBar}
+              series={chartOptions.seriesBar}
+              type="bar"
+              height={"100%"}
+              width={"100%"}
+            />
+          )}
+        </div>
+      </div>
     </div>
   );
 };
