2e2e8e67a5cbcfd1fe365359130bc07f84ffd311
namke-dev <namke.dev@gmail.com>
2024-01-11 20:19:05 +0700

    format code

--- src/components/footer.js
+++ src/components/footer.js
@@ -5,9 +5,9 @@ export default function Footer() {
     <footer>
       <div className="flex flex-col px-5 md:px-14 py-7 mt-6 bg-black text-white/70 text-sm gap-0">
         <p className="font-semibold pb-2">DEV STOCK - Project by @namke.dev</p>
-        <p>
-          FEATURE: Real-time Stock Price, Historycal Data, Market Trend,
-          company's financial data
+        <p className="pb-2">
+          FEATURE: Real-time Stock Price, Historycal Data, Market Trends,
+          Companies Financial Report
         </p>
         <p>
           Contact me via <span className="underline">namke.dev@gmail.com</span>
--- src/components/header.js
+++ src/components/header.js
@@ -1,27 +1,5 @@
 "use client";
-
-import React, { useEffect, useState } from "react";
-
 export default function Header() {
-  const [isLargeDevice, setIsLargeDevice] = useState(true);
-
-  useEffect(() => {
-    const checkWindowSize = () => {
-      setIsLargeDevice(window.innerWidth > 768);
-    };
-
-    // Initial check
-    checkWindowSize();
-
-    // Event listener for window resize
-    window.addEventListener("resize", checkWindowSize);
-
-    // Cleanup the event listener on component unmount
-    return () => {
-      window.removeEventListener("resize", checkWindowSize);
-    };
-  }, []);
-
   return (
     <header>
       <div className="flex flex-row flex-wrap items-center justify-start bg-black/80 text-gray-300 text-sm px-3 py-0.5 gap-5">
